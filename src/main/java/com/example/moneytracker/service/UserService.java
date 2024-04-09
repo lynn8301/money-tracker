0ecb73f12bd9da9f26f0855e98bee123ec3298d2
lynn8301 <lynn8301@gmail.com>
2024-04-09 13:55:06 +0800

    Modify code

--- src/main/java/com/example/moneytracker/service/UserService.java
+++ src/main/java/com/example/moneytracker/service/UserService.java
@@ -5,10 +5,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.crossstore.ChangeSetPersister.NotFoundException;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
-import org.springframework.web.server.ResponseStatusException;
 
 import com.example.moneytracker.model.User;
 import com.example.moneytracker.repository.UserRepository;
